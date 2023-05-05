92545732314e3a4114c28201693692c20fb229a1
Kirill Nazarov <deeplogic2000@gmail.com>
2023-05-05 12:59:18 +0300

    feat: add decision of 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const { rm, mkdir, readdir, copyFile, unlink } = require("node:fs/promises");
+const { join } = require("node:path");
 
+const sourceDir = join(__dirname, "files");
+const destinationDir = join(__dirname, "files-copy");
+
+const copyDir = async (sourceDir, destinationDir) => {
+  await rm(destinationDir, { recursive: true, force: true });
+  await mkdir(destinationDir, { recursive: true });
+
+  const sourceFiles = await readdir(sourceDir);
+
+  copyFiles(sourceFiles);
+};
+
+const copyFiles = async (sourceFiles) => {
+  for (const file of sourceFiles) {
+    await copyFile(join(sourceDir, file), join(destinationDir, file));
+  }
+};
+
+copyDir(sourceDir, destinationDir);
